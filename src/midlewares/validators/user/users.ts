661360a48e44b3a5fc376b6ed8495f959efdac5d
jose bernal <jose12_bernal@hotmail.com>
2024-06-19 16:33:00 -0700

    improve

--- src/midlewares/validators/user/users.ts
+++ src/midlewares/validators/user/users.ts
@@ -4,7 +4,7 @@ import validateResult from '../validationResult';
 
 export const userCreated = [
     check('name').exists().notEmpty().withMessage('el nombre no puede ir vacio'),
-    check('email').exists().isEmail().notEmpty().withMessage('el email no puede ir vacio'),
+    check('email').exists().isEmail().notEmpty().isEmail().normalizeEmail().withMessage('el email no puede ir vacio'),
     check('password').exists().notEmpty().withMessage('el password no puede ir vacio'),
     // check('password_confirmation').exists().notEmpty().withMessage('el confirmar password no puede ir vacio'),
     (req: Request, res: Response, next: NextFunction) => {
